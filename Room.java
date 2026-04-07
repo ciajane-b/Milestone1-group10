d62361ae578c3baf7060273fc07fd8b6738e43a0
AldCream <aldrianmaure@gmail.com>
2026-04-07 21:25:46 +0800

    Add Room class with properties and methods

---
+++ Room.java
@@ -0,0 +1,13 @@
+public class Room {
+    private String roomNumber;
+    private String status;
+
+    public Room(String roomNumber, String status) {
+        this.roomNumber = roomNumber;
+        this.status = status;
+    }
+
+    public String getRoomNumber() { return roomNumber; }
+    public String getStatus()     { return status;     }
+    public void setStatus(String status) { this.status = status; }
+}
